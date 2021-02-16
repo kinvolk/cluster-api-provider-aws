d9abad46e11121bea20720eea4b7630793351324
Mateusz Gozdek <mateusz@kinvolk.io>
2021-02-16 09:38:31 +0100

    api/v1alpha3: add S3Bucket field to AWSClusterSpec
    As a preparation for using it as an alternative backend to Secret
    Manager for OS-es, which do not support Secret Manager, like Flatcar
    Container Linux.
    
    Co-authored-by: Dongsu Park <dongsu@kinvolk.io>
    Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

--- api/v1alpha2/zz_generated.conversion.go
+++ api/v1alpha2/zz_generated.conversion.go
@@ -452,6 +452,7 @@ func autoConvert_v1alpha3_AWSClusterSpec_To_v1alpha2_AWSClusterSpec(in *v1alpha3
 	// WARNING: in.ImageLookupBaseOS requires manual conversion: does not exist in peer-type
 	// WARNING: in.Bastion requires manual conversion: does not exist in peer-type
 	// WARNING: in.IdentityRef requires manual conversion: does not exist in peer-type
+	// WARNING: in.S3Bucket requires manual conversion: does not exist in peer-type
 	return nil
 }
 
